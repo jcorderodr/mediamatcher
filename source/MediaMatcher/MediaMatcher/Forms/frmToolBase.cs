c89d758cc79d54c7bd6f1e1c5584d8b25a0207bb
José Cordero <jcorderodr@users.noreply.github.com>
2013-07-29 00:16:27 -0400

    Changing Dev Machine - Committing all

---
+++ source/MediaMatcher/MediaMatcher/Forms/frmToolBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MediaMatcher.Forms
+{
+    public partial class frmToolBase : frmBase
+    {
+        public frmToolBase()
+        {
+            InitializeComponent();
+        }
+    }
+}
